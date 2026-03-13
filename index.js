f2a4d8aa6643b1753b03ee1c3502f8bf37aba2db
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-13 18:51:12 +0530

    geratePassword function and useEffect is done

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-import React,{useState} from "react"
+import React,{useEffect, useState} from "react"
 import ReactDOM from "react-dom/client"
 
 function PasswordGenerator(){
-    const [password,setPassword]=useState("ABFG"); 
-    const [length,setLength]=useState(5);
+    const [password,setPassword]=useState(""); 
+    const [length,setLength]=useState(1);
     const [number,setNumber]=useState(false);
     const [character,setCharacter]=useState(false);
 
+    function generatePassword(){
+        let str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const nums="0123456789";
+        const chars="!~()*&^%$#@*-+/";
+
+        if(number){
+            str+=nums;
+        }
+        if(character){
+            str+=chars;
+        }
+
+        let pass="";
+        for(let i=0;i<length;i++){
+            pass+=str[Math.floor(Math.random()*str.length)];
+        }
+
+        setPassword(pass);
+    }
+
+    useEffect(()=>{
+        generatePassword();
+    },[length,number,character])
+
     return (
         <>
-        
         <div id="container">
             <h1 style={{fontSize:"48px"}}>{password}</h1>
-            <input style={{width:"100px"}} type="range" min={5} max={50} value={length} onChange={(e)=>setLength(e.target.value)}></input>
-            <label >Length({length})</label>
-            <input defaultChecked={number} type="checkbox" onChange={()=>setNumber(!number)}></input>
-            <label >Numbers</label>
-            <input defaultChecked={character} type="checkbox" onChange={()=>setCharacter(!character)}></input>
-            <label >Characters</label>
+
+            <input 
+                style={{width:"100px"}} type="range" min={1} max={50} value={length} 
+onChange={(e)=>setLength(e.target.value)}
+            ></input>
+            <label>Length({length})</label>
+
+            <input checked={number} type="checkbox" onChange={()=>setNumber(!number)}></input>
+            <label>Numbers</label>
+
+            <input checked={character} type="checkbox" onChange={()=>setCharacter(!character)}></input>
+            <label>Characters</label>
         </div>
         </>
     )
-
-
-
 }
 
 const reactRoot=ReactDOM.createRoot(document.getElementById('root'));
